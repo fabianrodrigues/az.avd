793d3cce1bdb633aaa9e8c27b6e68c450609ba1a
fabianrodrigues <80415510+fabianrodrigues@users.noreply.github.com>
2021-10-12 16:01:25 +0100

    Add files via upload

---
+++ az.AVD Move Session Host to another host pool.ps1
@@ -0,0 +1,2 @@
+﻿Move-AvdSessionhost -FromHostpoolName vdhp-eng-pp-uksouth-01 -FromResourceGroupName rg-wvd-pool-eng-pp-uksouth-01 -ToHostpoolName vdhp-dev-pp-uksouth-02 -ToResourceGroupName rg-wvd-pool-dev-pp-uksouth-02 -SessionHostName vmtest-eng-12.int.nihp.nhs.uk
+Update-AvdSessionhost -HostpoolName vdhp-eng-pp-uksouth-01 -ResourceGroupName rg-wvd-pool-eng-pp-uksouth-01 -SessionHostName vmtest-eng-12.int.nihp.nhs.uk -AllowNewSession $true -assignedUser fabian.rodrigues.support@test-and-trace.nhs.uk
