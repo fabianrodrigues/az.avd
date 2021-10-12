3295e410157ea02e0b4e64fab29dc04488601c32
fabianrodrigues <80415510+fabianrodrigues@users.noreply.github.com>
2021-10-12 16:02:07 +0100

    Delete Move Session Host to another host pool.ps1

--- Move Session Host to another host pool.ps1
+++
@@ -1,1 +0,0 @@
-﻿Move-AvdSessionhost -FromHostpoolName vdhp-eng-pp-uksouth-01 -FromResourceGroupName rg-wvd-pool-eng-pp-uksouth-01 -ToHostpoolName vdhp-dev-pp-uksouth-02 -ToResourceGroupName rg-wvd-pool-dev-pp-uksouth-02 -SessionHostName vmtest-eng-12.int.nihp.nhs.uk
