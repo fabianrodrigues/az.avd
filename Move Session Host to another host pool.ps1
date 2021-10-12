4d47434dd986b30edddb82af2a9bd2b04696e83b
fabianrodrigues <80415510+fabianrodrigues@users.noreply.github.com>
2021-10-12 15:37:04 +0100

    Add files via upload

---
+++ Move Session Host to another host pool.ps1
@@ -0,0 +1,1 @@
+﻿Move-AvdSessionhost -FromHostpoolName vdhp-eng-pp-uksouth-01 -FromResourceGroupName rg-wvd-pool-eng-pp-uksouth-01 -ToHostpoolName vdhp-dev-pp-uksouth-02 -ToResourceGroupName rg-wvd-pool-dev-pp-uksouth-02 -SessionHostName vmtest-eng-12.int.nihp.nhs.uk
